ee83a3ea0eed4ba240e50c958ce0c9b64b23eb35
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2025-08-08 22:20:41 +0900

    create article entity, repository, service (#3)
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

---
+++ src/main/java/com/example/elasticsearch/article/entity/Article.java
@@ -0,0 +1,20 @@
+package com.example.elasticsearch.article.entity;
+
+import com.example.elasticsearch.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Article extends BaseEntity {
+    private String title;
+    private String content;
+}
