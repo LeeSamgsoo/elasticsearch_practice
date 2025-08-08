9cedf30227b883f8d1cf92caa77a9c7aaffc3fc0
abc <soo990414@edu.dju.ac.kr>
2025-08-08 22:19:31 +0900

    create article entity, repository, service

---
+++ src/main/java/com/example/elasticsearch/article/service/ArticleService.java
@@ -0,0 +1,11 @@
+package com.example.elasticsearch.article.service;
+
+import com.example.elasticsearch.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+}
