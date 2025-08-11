cfec6a0469abfc4bd9a3b50ab9a4094c4255854a
abc <soo990414@edu.dju.ac.kr>
2025-08-11 19:42:18 +0900

    article list instant

--- src/main/java/com/example/elasticsearch/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/elasticsearch/article/controller/ApiV1ArticleController.java
@@ -1,8 +1,13 @@
 package com.example.elasticsearch.article.controller;
 
+import com.example.elasticsearch.article.dto.ArticleDTO;
+import com.example.elasticsearch.article.entity.Article;
 import com.example.elasticsearch.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,8 +16,18 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")
-    public String getArticles() {
-        return "목록";
+    public List<ArticleDTO> getArticles() {
+        List<ArticleDTO> articleDTOList = new ArrayList<>();
+
+        Article article1 = new Article("제목1", "내용1");
+        Article article2 = new Article("제목2", "내용2");
+        Article article3 = new Article("제목3", "내용3");
+
+        articleDTOList.add(new ArticleDTO(article1));
+        articleDTOList.add(new ArticleDTO(article2));
+        articleDTOList.add(new ArticleDTO(article3));
+
+        return articleDTOList;
     }
 
     @GetMapping("/{id}")
