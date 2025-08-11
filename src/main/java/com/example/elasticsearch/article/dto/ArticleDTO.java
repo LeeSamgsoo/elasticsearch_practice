2a33c2b295cb17c82a4b56fc0244b80492190482
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2025-08-11 19:50:42 +0900

    Feature/article mvc (#6)
    * create article entity, repository, service
    
    * article controller REST api create
    
    * rest api controller method create
    
    * article list instant
    
    ---------
    
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

---
+++ src/main/java/com/example/elasticsearch/article/dto/ArticleDTO.java
@@ -0,0 +1,23 @@
+package com.example.elasticsearch.article.dto;
+
+import com.example.elasticsearch.article.entity.Article;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ArticleDTO {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime modifiedDate;
+
+    public ArticleDTO(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdDate = article.getCreatedDate();
+        this.modifiedDate = article.getModifiedDate();
+    }
+}
