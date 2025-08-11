19be6e4a63e7fc501bf9e1f80f98e63636091c6e
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2025-08-11 20:26:08 +0900

    Feature/article mvc (#10)
    * create article entity, repository, service
    
    * article controller REST api create
    
    * rest api controller method create
    
    * article list instant
    
    * article one by one
    
    * article create instant
    
    * line clear
    
    ---------
    
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

--- src/main/java/com/example/elasticsearch/article/service/ArticleService.java
+++ src/main/java/com/example/elasticsearch/article/service/ArticleService.java
@@ -1,5 +1,7 @@
 package com.example.elasticsearch.article.service;
 
+import com.example.elasticsearch.article.dto.ArticleDTO;
+import com.example.elasticsearch.article.entity.Article;
 import com.example.elasticsearch.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    public ArticleDTO articleCreate(String title, String content) {
+        Article article = Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+        this.articleRepository.save(article);
+        return new ArticleDTO(article);
+    }
 }
