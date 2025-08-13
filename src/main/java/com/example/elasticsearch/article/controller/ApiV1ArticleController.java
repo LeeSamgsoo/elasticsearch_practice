cd2a9b3a916b144b90975b3f8c1dd85fba618554
abc <soo990414@edu.dju.ac.kr>
2025-08-13 15:15:11 +0900

    article read change use RsData

--- src/main/java/com/example/elasticsearch/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/elasticsearch/article/controller/ApiV1ArticleController.java
@@ -1,12 +1,15 @@
 package com.example.elasticsearch.article.controller;
 
+import co.elastic.clients.elasticsearch.nodes.Http;
 import com.example.elasticsearch.article.dto.ArticleDTO;
-import com.example.elasticsearch.article.entity.Article;
+import com.example.elasticsearch.article.response.ArticleResponse;
+import com.example.elasticsearch.article.response.ArticlesResponse;
 import com.example.elasticsearch.article.service.ArticleService;
+import com.example.elasticsearch.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,13 +19,35 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")
-    public List<ArticleDTO> getArticles() {
-        return this.articleService.getArticles();
+    public RsData<ArticlesResponse> getArticles() {
+        List<ArticleDTO> articleDTOList = this.articleService.getArticles();
+        if (articleDTOList.isEmpty()) {
+            return RsData.of(
+                    "401",
+                    "게시글이 존재하지 않습니다."
+            );
+        }
+        return RsData.of(
+                "200",
+                "다건 조회 성공",
+                new ArticlesResponse(articleDTOList)
+        );
     }
 
     @GetMapping("/{id}")
-    public ArticleDTO getArticle(@PathVariable(value = "id") Long id) {
-        return this.articleService.getArticle(id);
+    public RsData<ArticleResponse> getArticle(@PathVariable(value = "id") Long id) {
+        ArticleDTO articleDTO = this.articleService.getArticle(id);
+        if (articleDTO == null) {
+            return RsData.of(
+              "401",
+              "존재하지 않는 게시글 입니다."
+            );
+        }
+        return RsData.of(
+                "200",
+                "단건 조회 성공",
+                new ArticleResponse(articleDTO)
+        );
     }
 
     @PostMapping("")
