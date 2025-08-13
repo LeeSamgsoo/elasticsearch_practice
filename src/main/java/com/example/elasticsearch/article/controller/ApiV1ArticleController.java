e28466b23254ee7a2d5d3bff7d05d570d140de1b
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2025-08-13 16:50:14 +0900

    Feature/article req res (#16)
    * article read change use RsData
    
    * article CRUD use RsData and Req, Res
    
    * line clearing
    
    * apply swagger
    
    * line clearing
    
    ---------
    
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

--- src/main/java/com/example/elasticsearch/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/elasticsearch/article/controller/ApiV1ArticleController.java
@@ -1,32 +1,36 @@
 package com.example.elasticsearch.article.controller;
 
-import co.elastic.clients.elasticsearch.nodes.Http;
 import com.example.elasticsearch.article.dto.ArticleDTO;
 import com.example.elasticsearch.article.request.ArticleCreateRequest;
 import com.example.elasticsearch.article.request.ArticleModifyRequest;
 import com.example.elasticsearch.article.response.*;
 import com.example.elasticsearch.article.service.ArticleService;
 import com.example.elasticsearch.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/articles")
+@RequestMapping(value = "api/v1/articles", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@Tag(name = "ApiV1ArticleController", description = "게시물 CRUD API")
 public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")
+    @Operation(summary = "게시글 다건 조회")
     public RsData<ArticlesResponse> getArticles() {
         List<ArticleDTO> articleDTOList = this.articleService.getArticles();
         if (articleDTOList.isEmpty()) {
             return RsData.of(
-                    "401",
+                    "500",
                     "게시글이 존재하지 않습니다."
             );
         }
@@ -38,12 +42,13 @@ public class ApiV1ArticleController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 단건 조회")
     public RsData<ArticleResponse> getArticle(@PathVariable(value = "id") Long id) {
         ArticleDTO articleDTO = this.articleService.getArticle(id);
         if (articleDTO == null) {
             return RsData.of(
-                    "401",
-                    "존재하지 않는 게시글 입니다."
+                    "500",
+                    "%d번 게시글은 존재하지 않습니다.".formatted(id)
             );
         }
         return RsData.of(
@@ -54,6 +59,7 @@ public class ApiV1ArticleController {
     }
 
     @PostMapping("")
+    @Operation(summary = "게시글 생성")
     public RsData<ArticleCreateResponse> createArticle(@Valid @RequestBody ArticleCreateRequest articleCreateRequest) {
         ArticleDTO articleDTO = this.articleService.articleCreate(
                 articleCreateRequest.getTitle(),
@@ -67,6 +73,7 @@ public class ApiV1ArticleController {
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "게시글 수정")
     public RsData<ArticleModifyResponse> modifyArticle(@PathVariable(value = "id") Long id,
                                                        @Valid @RequestBody ArticleModifyRequest articleModifyRequest) {
         ArticleDTO articleDTO = this.articleService.articleModify(
@@ -76,8 +83,8 @@ public class ApiV1ArticleController {
         );
         if (articleDTO == null) {
             return RsData.of(
-                    "401",
-                    "존재하지 않는 게시글 입니다."
+                    "500",
+                    "%d번 게시글은 존재하지 않습니다.".formatted(id)
             );
         }
         return RsData.of(
@@ -88,12 +95,13 @@ public class ApiV1ArticleController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제")
     public RsData<ArticleDeleteResponse> deleteArticle(@PathVariable(value = "id") Long id) {
         ArticleDTO articleDTO = this.articleService.articleDelete(id);
         if (articleDTO == null) {
             return RsData.of(
-                    "400",
-                    "존재하지 않는 게시글 입니다."
+                    "500",
+                    "%d번 게시글은 존재하지 않습니다.".formatted(id)
             );
         }
         return RsData.of(
