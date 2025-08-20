25f8ff1efdd06f03823edae6f5fb8f4237816fc1
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2025-08-20 23:27:43 +0900

    Feature/jwt (#21)
    * member mvc
    
    * member dto modify
    
    * article entity add author
    
    * member request create
    
    ---------
    
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

---
+++ src/main/java/com/example/elasticsearch/domain/member/request/MemberRequest.java
@@ -0,0 +1,12 @@
+package com.example.elasticsearch.domain.member.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class MemberRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
