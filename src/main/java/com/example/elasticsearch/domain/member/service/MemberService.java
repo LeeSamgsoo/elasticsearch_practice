3255600aef11e67f4d30726636f4b598d4037a78
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2025-08-22 22:46:27 +0900

    Feature/jwt (#23)
    * member mvc
    
    * member dto modify
    
    * article entity add author
    
    * member request create
    
    * security config create
    
    * member join
    
    ---------
    
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

--- src/main/java/com/example/elasticsearch/domain/member/service/MemberService.java
+++ src/main/java/com/example/elasticsearch/domain/member/service/MemberService.java
@@ -1,19 +1,22 @@
 package com.example.elasticsearch.domain.member.service;
 
-import com.example.elasticsearch.domain.member.dto.MemberDTO;
 import com.example.elasticsearch.domain.member.entity.Member;
 import com.example.elasticsearch.domain.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    public void memberJoin(String username, String password) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .build();
+        this.memberRepository.save(member);
+    }
 }
