ee9730a794171ce8d82564785aa3ca29e1c66839
abc <soo990414@edu.dju.ac.kr>
2025-08-22 22:45:48 +0900

    member join

--- src/main/java/com/example/elasticsearch/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/elasticsearch/domain/member/controller/ApiV1MemberController.java
@@ -4,6 +4,7 @@ import com.example.elasticsearch.domain.article.request.ArticleCreateRequest;
 import com.example.elasticsearch.domain.article.request.ArticleModifyRequest;
 import com.example.elasticsearch.domain.article.response.*;
 import com.example.elasticsearch.domain.member.dto.MemberDTO;
+import com.example.elasticsearch.domain.member.request.MemberRequest;
 import com.example.elasticsearch.domain.member.service.MemberService;
 import com.example.elasticsearch.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,4 +24,13 @@ import java.util.stream.Collectors;
 @Tag(name = "ApiV1MemberController", description = "회원 인증 인가 API")
 public class ApiV1MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/join")
+    public String memberJoin (@Valid @RequestBody MemberRequest memberRequest) {
+        this.memberService.memberJoin(
+                memberRequest.getUsername(),
+                memberRequest.getPassword()
+        );
+        return "가입완료";
+    }
 }
