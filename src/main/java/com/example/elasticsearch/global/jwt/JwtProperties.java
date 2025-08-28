1c0fde3fe37db1dd73bb075603dff161ad9f489d
abc <soo990414@edu.dju.ac.kr>
2025-08-28 15:28:15 +0900

    jwt config

---
+++ src/main/java/com/example/elasticsearch/global/jwt/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.example.elasticsearch.global.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "custom.jwt")
+public class JwtProperties {
+    private String secretKey;
+}
